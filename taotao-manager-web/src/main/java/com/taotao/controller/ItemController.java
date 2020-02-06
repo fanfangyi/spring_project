0f6021cbb4833945dea50a7f8d7683f473b18ed9
fanfangyi <1021816076@qq.com>
2020-02-06 14:40:49 +0800

    web 更新

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
@@ -20,5 +21,13 @@ public class ItemController {
 	{
 		TbItem tbItem = itemService.getItemById(itemId);
 		return tbItem;
+	}
+	
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(int page,int rows)
+	{
+		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		return result;
 	}
 }
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -1,10 +1,16 @@
 package com.taotao.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -15,4 +21,19 @@ public class ItemServiceImpl implements ItemService {
 		 TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
 		 return tbItem;
 	 }
+	@Override
+	public EasyUIDataGridResult getItemList(int page,int rows)
+	{
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//获取查询结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setTotal(pageInfo.getTotal());
+		
+		return result;
+	}
 }
